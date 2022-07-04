f5601dc7dadf967e013f94f7bd49efdebb2ca715
Liam Bell <liam.bell@leadingedje.com>
2022-07-04 16:06:55 -0400

    Add comments to def adapter

--- src/main/java/com/example/msip/message/DefaultMessageAdapter.java
+++ src/main/java/com/example/msip/message/DefaultMessageAdapter.java
@@ -26,13 +26,16 @@ public class DefaultMessageAdapter implements IMessageAdapter{
         MessageConsumer consumer = null;
 
         try {
-            JmsFactoryFactory factory = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER);
             MQQueueConnectionFactory connectionFactory = new MQQueueConnectionFactory();
 
+            //Host name will reflect the host ip of the IBM Websphere server
             connectionFactory.setStringProperty(WMQConstants.WMQ_HOST_NAME, "localhost");
+            //Port will probably be 1414 but I'd ask
             connectionFactory.setIntProperty(WMQConstants.WMQ_PORT, 1414);
+            //Need to get the channel name if it's not default
             connectionFactory.setStringProperty(WMQConstants.WMQ_CHANNEL, "SYSTEM.DEF.SVRCONN");
             connectionFactory.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
+            //Get the QUEUE MANAGER NAME. PROBABLY WONT BE QM1
             connectionFactory.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, "QM1");
             connectionFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
 
